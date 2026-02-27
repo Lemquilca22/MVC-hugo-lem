175fc1a17ca3af8d3209e3fe2fe8164a69b97b7d
Lemsito22 <lemquilca22@gmail.com>
2026-02-27 14:13:33 +0100

    .

--- Java/src/Controlador/ComidaControlador.java
+++ Java/src/Controlador/ComidaControlador.java
@@ -6,4 +6,13 @@ import Vista.ComidaVista;
 public class ComidaControlador {
     private Comida modelo;
     private ComidaVista vista;
+
+    public ComidaControlador(Comida modelo, ComidaVista vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+    }
+
+    public void iniciar(){
+        vista.mostrarMensaje("Registro de comida y bebida");
+    }
 }
--- Java/src/Vista/ComidaVista.java
+++ Java/src/Vista/ComidaVista.java
@@ -3,5 +3,30 @@ package Vista;
 import java.util.Scanner;
 
 public class ComidaVista {
+    private Scanner scanner= new Scanner(System.in);
+
+    public String pedirnombre(){
+        return scanner.nextLine();
+    }
+    public String pedirTamanio(){
+        return scanner.nextLine();
+    }
+    public String pedirBebida(){
+        return scanner.nextLine();
+    }
+    public String pedirtamioBebida(){
+        return scanner.nextLine();
+    }
+
+    public void mostrarComida(String nombre, String tamanio, String bebida, String tamanioBebida){
+        System.out.println("\n---Datos de la orden---");
+        System.out.println("Hamburguesa: "+nombre);
+        System.out.println("Tamaño: "+tamanio);
+        System.out.println("Bebida: "+bebida);
+        System.out.println("Tamaño: "+tamanioBebida);
+    }
+    public void mostrarMensaje(String mensaje){
+        System.out.println(mensaje);
+    }
 
 }
