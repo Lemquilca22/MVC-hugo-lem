d5f8a2ac1426d51bdac7c294ce45e4ace7d01f11
Lemsito22 <lemquilca22@gmail.com>
2026-03-04 09:02:05 +0100

    .

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,9 +1,16 @@
+import Controlador.ComidaControlador;
 import Controlador.PersonaControlador;
+import Modelo.Comida;
 import Modelo.Persona;
+import Vista.ComidaVista;
 import Vista.PersonaVista;
 
 public class Main {
     public static void main (String[] args) {
+        Comida modelo= new Comida(0,0);
+        ComidaVista vista = new ComidaVista();
+        ComidaControlador controlador= new ComidaControlador(modelo, vista);
+        controlador.iniciar();
 
     }
 }
